5ad8d7a29977149d81d389c93193f176f48c64ce
tmsouthwell <goldtoneconsulting@gmail.com>
2022-03-12 15:20:10 -0800

    First Change

--- server.py
+++ server.py
@@ -1,35 +1,51 @@
-from flask import Flask  # Import Flask to allow us to create our app
+from flask import Flask, render_template  # Import Flask to allow us to create our app
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
-@app.route('/')          # The "@" decorator associates this route with the function immediately following
 
-def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
+# @app.route('/')          # The "@" decorator associates this route with the function immediately following
+# def hello_world():
+#     return 'Hello World!'  # Return the string 'Hello World!' as a response
 
-@app.route('/dojo')
-def dojo():
-    return "Dojo!"
-
-
-@app.route('/say/<name>')  
-def say(name):
-    print(name)
-    return "Hi, " + name
+# @app.route('/dojo')
+# def dojo():
+#     return "Dojo!"
 
 
-@app.route('/success')
-def success():
-    return "success"
+# @app.route('/say/<name>')  
+# def say(name):
+#     print(name)
+#     return "Hi, " + name
 
-@app.route('/hello/<name>/<int:num>') # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
-def hello(name, num):
-    print(name, num)
-    return f"{name * num}"
 
-@app.route('/users/<username>/<id>') # for a route '/users/____/____', two parameters in the url get passed as username and id
-def show_user_profile(username, id):
-    print(username)
-    print(id)
-    return "username: " + username + ", id: " + id
+# @app.route('/success')
+# def success():
+#     return "success"
+
+# @app.route('/hello/<name>/<int:num>') # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
+# def hello(name, num):
+#     print(name, num)
+#     output = ''
+#     for i in range(0, num):
+#         output += f"<p>{name.capitalize()}</p>"
+#     return output
+
+# @app.route('/users/<username>/<id>') # for a route '/users/____/____', two parameters in the url get passed as username and id
+# def show_user_profile(username, id):
+#     print(username)
+#     print(id)
+#     return "username: " + username + ", id: " + id
+
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+    {'name' : 'Michael', 'age' : 35},
+    {'name' : 'John', 'age' : 30 },
+    {'name' : 'Mark', 'age' : 25},
+    {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], students = student_info)
+
+
 
 
 
